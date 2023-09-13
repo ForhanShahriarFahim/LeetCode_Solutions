de3c860fa87571700e4c72a09f6d52cbfe493850
Forhan Shahriar Fahim <47686584+ForhanShahriarFahim@users.noreply.github.com>
2023-09-13 08:50:47 +0600

    Time: 6 ms (51.84%), Space: 7.8 MB (93.23%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,10 +1,13 @@
 class Solution {
 public:
     vector<int> countBits(int n) {
-        vector<int>ans;
-        for(int i = 0;i<=n;i++){
-            int value = __builtin_popcount(i);
-            ans.push_back(value);
+        vector<int>ans(n+1);
+        ans[0] = 0;
+        for(int i = 1;i<=n;i++){
+            if(i%2){
+                ans[i] = 1+ans[i/2];
+            }
+            else ans[i] = ans[i/2];
         }
         return ans;
     }
