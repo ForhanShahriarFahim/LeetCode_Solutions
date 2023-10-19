a82bce1ac3ec05daf13906aaf8ee4933cc919620
Forhan Shahriar Fahim <47686584+ForhanShahriarFahim@users.noreply.github.com>
2023-10-20 00:30:19 +0600

    Time: 163 ms (52.57%), Space: 140.9 MB (7.55%) - LeetHub

---
+++ 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+        vector<int>pos,neg;
+        for(int i = 0;i<nums.size();i++){
+            if(nums[i]>0)pos.push_back(nums[i]);
+            else{
+                neg.push_back(nums[i]);
+            }
+        }
+        vector<int>ans;
+        for(int i = 0;i<nums.size()/2;i++){
+            ans.push_back(pos[i]);
+            ans.push_back(neg[i]);
+        }
+        return ans;
+    }
+};
