b702bcdb4ab4ff3e1a6d3a402a87fb145de54b28
Forhan Shahriar Fahim <47686584+ForhanShahriarFahim@users.noreply.github.com>
2024-04-13 12:21:57 +0600

    Time: 22 ms (31.23%), Space: 24.6 MB (85.75%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -12,21 +12,13 @@ private:
             if(arr[mid]!=arr[mid+1] && arr[mid]!=arr[mid-1]){
                 return arr[mid];
             }            
-            if(mid%2)
+            if((mid%2 &&arr[mid] == arr[mid-1])||(mid%2==0 && arr[mid] == arr[mid+1]))
             {
-                if(arr[mid] == arr[mid-1]){
                 low = mid+1;
-            }
-                else 
-                    high = mid-1;
             
             }
             else {
-                if(arr[mid] == arr[mid-1]){
-                    high = mid-1;
-                }
-                else 
-                    low = mid+1;
+                high = mid-1;
             }
             
         }
