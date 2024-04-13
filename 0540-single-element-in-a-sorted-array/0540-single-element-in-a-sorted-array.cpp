3a04605281e0cfc861873a434c3ca9aa4ab2f700
Forhan Shahriar Fahim <47686584+ForhanShahriarFahim@users.noreply.github.com>
2024-04-13 12:17:49 +0600

    Time: 19 ms (54.56%), Space: 24.6 MB (85.75%) - LeetHub

---
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -0,0 +1,41 @@
+class Solution {
+private:
+    int SingleElement(vector<int>&arr){   
+        int n = arr.size();
+        if(n==1)return arr[0];
+        if(arr[0]!=arr[1])return arr[0];
+        if(arr[n-1]!=arr[n-2])return arr[n-1];
+        int low = 1, high = arr.size()-2;
+        
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(arr[mid]!=arr[mid+1] && arr[mid]!=arr[mid-1]){
+                return arr[mid];
+            }            
+            if(mid%2)
+            {
+                if(arr[mid] == arr[mid-1]){
+                low = mid+1;
+            }
+                else 
+                    high = mid-1;
+            
+            }
+            else {
+                if(arr[mid] == arr[mid-1]){
+                    high = mid-1;
+                }
+                else 
+                    low = mid+1;
+            }
+            
+        }
+        return -1;
+        
+    }
+ 
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        return SingleElement(nums);
+    }
+};
