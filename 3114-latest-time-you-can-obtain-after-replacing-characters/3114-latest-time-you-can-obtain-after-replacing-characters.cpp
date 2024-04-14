473c460c8e0fef52088467fcfb779ae96b12eedb
Forhan Shahriar Fahim <47686584+ForhanShahriarFahim@users.noreply.github.com>
2024-04-14 11:26:18 +0600

    Time: 0 ms (100.00%), Space: 7.4 MB (100.00%) - LeetHub

---
+++ 3114-latest-time-you-can-obtain-after-replacing-characters/3114-latest-time-you-can-obtain-after-replacing-characters.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    string findLatestTime(string s) {
+        if(s[0] == '?' && (s[1] == '0' || s[1] == '1'||s[1] == '?')) s[0] = '1';
+        else if(s[0] == '?') s[0] = '0';
+        if(s[1] == '?' && s[0] == '1') s[1] = '1';
+        else if(s[1] == '?' && s[0] == '0') s[1] = '9';
+        if(s[3] == '?') s[3] = '5';
+        if(s[4] == '?') s[4] = '9';
+        return s;
+    }
+};
