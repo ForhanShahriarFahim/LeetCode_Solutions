653e6181ec7a33078b034a825eafd515c12d301b
Forhan Shahriar Fahim <47686584+ForhanShahriarFahim@users.noreply.github.com>
2024-07-13 23:38:37 +0600

    Time: 0 ms (100%), Space: 11.6 MB (6.91%) - LeetHub

--- 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.cpp
+++ 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.cpp
@@ -5,16 +5,10 @@ public:
         for(auto x:arr){
             mp[x]++;
         }
-        vector<int>v;
-        for(auto x:mp){
-            v.push_back(x.second);
-            
-        }
-        sort(v.begin(),v.end());
-        for(int i = 0;i<v.size()-1;i++){
-            if(v[i] == v[i+1]){
-                return false;
-            }
+        unordered_map<int,int>ans;
+        for(auto&x:mp){
+            ans[x.second]++;
+            if(ans[x.second]>1)return false;
         }
         return true;
     }
