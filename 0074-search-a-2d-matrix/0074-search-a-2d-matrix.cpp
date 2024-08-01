9583d54608470fcba051ac0509ef55679dff96b1
Forhan Shahriar Fahim <47686584+ForhanShahriarFahim@users.noreply.github.com>
2024-08-01 12:03:39 +0600

    Time: 0 ms (100%), Space: 12 MB (36.32%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,26 +1,20 @@
 class Solution {
 public:
-    bool binary_search(vector<int>&nums, int target){
-        int high = nums.size()-1;
-        int low = 0;
-        while(low<=high){
-            int mid = (low + high)/2;
-            if(nums[mid] == target) return true;
-            if(nums[mid]<target){
-                low = mid+1;
-            }
-            else high = mid-1;
-        }
-        return false;
-    }
+    
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int n = matrix.size();
         int m = matrix[0].size();
-        for(int i = 0;i<n;i++){
-            if(matrix[i][0]<=target && target<=matrix[i][m-1]){
-                return binary_search(matrix[i],target);
+        int low = 0, high = n*m-1;
+        while(low<=high){
+            int mid = (low + high)/2;
+            int row = mid/m, col = mid%m;
+            if(matrix[row][col] == target){
+                return true;
             }
-            else continue;            
+            if(matrix[row][col]<target){
+                low = mid+1;
+            }
+            else high = mid-1;
         }
         return false;
     }
