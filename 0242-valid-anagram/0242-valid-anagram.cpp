9d5466c90722be6cbd8c3f128e75e45c43a491fd
Forhan Shahriar Fahim <47686584+ForhanShahriarFahim@users.noreply.github.com>
2024-08-07 13:05:04 +0600

    Time: 8 ms (63.64%), Space: 8.8 MB (59.56%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        unordered_map<int,int>mp;
         if(s.length()!=t.length()) return false;
+        int freq[26] = {0};
         for(int i = 0;i<s.size();i++){
-            mp[s[i]]++;
+            freq[s[i]-97]++;
         }
         for(int i = 0;i<t.size();i++){
-            mp[t[i]]--;
+            freq[t[i]-97]--;
         }
         for(int i = 0;i<s.size();i++){
-            if(mp[s[i]]) return false;
+            if(freq[s[i]-97]) return false;
         }
         return true;
     }
