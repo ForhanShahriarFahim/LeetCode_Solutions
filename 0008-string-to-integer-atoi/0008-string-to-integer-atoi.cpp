78468224cd0e66a32d4be42dbb35896355a38dc0
Forhan Shahriar Fahim <47686584+ForhanShahriarFahim@users.noreply.github.com>
2024-08-08 22:29:19 +0600

    Time: 3 ms (58.22%), Space: 9.1 MB (33.62%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,33 +1,28 @@
 class Solution {
 public:
-    int myAtoi(string s) 
-    {
-        int i=0;
-        int sign=1;
-        long ans=0;
-        while(i<s.length() && s[i]==' ')
+    int myAtoi(string s) {
+        int n = s.size();
+        if(n == 0) return 0;
+        int i = 0;
+        while(i<n && s[i] == ' ') i++;
+        bool isNegative = false;
+        if(s[i] == '-') {
+            isNegative = true;
             i++;
-        if(s[i]=='-')
-        {
-            sign=-1;
+        }else if(s[i] == '+') i++;
+        int result = 0;
+        while(i<n && isDigit(s[i])){
+            int digit = s[i]-'0';
+            if(result>INT_MAX/10 || (result == (INT_MAX/10) && digit>7))
+                return isNegative? INT_MIN : INT_MAX;
+            
+            result = (result*10) + digit;
             i++;
         }
-        else if(s[i]=='+')
-            i++;
-        while(i<s.length())
-        {
-            if(s[i]>='0' && s[i]<='9')
-            {
-                ans=ans*10+(s[i]-'0');
-                if(ans>INT_MAX && sign==-1)
-                    return INT_MIN;
-                else if(ans>INT_MAX && sign==1)
-                    return INT_MAX;
-                i++;
-            }
-            else
-                return ans*sign;
-        }
-        return (ans*sign);
+        return isNegative? -result:result;
     }
+    private:
+        bool isDigit(char ch){
+            return ch>='0' && ch<='9';
+        }
 };
