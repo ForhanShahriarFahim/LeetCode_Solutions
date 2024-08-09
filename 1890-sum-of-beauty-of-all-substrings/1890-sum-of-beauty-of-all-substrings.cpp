dedea5cf2fedc143c9e165471eef74ac8fe29968
Forhan Shahriar Fahim <47686584+ForhanShahriarFahim@users.noreply.github.com>
2024-08-09 19:48:48 +0600

    Time: 133 ms (68.87%), Space: 26.3 MB (26.6%) - LeetHub

---
+++ 1890-sum-of-beauty-of-all-substrings/1890-sum-of-beauty-of-all-substrings.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int beautySum(string s) {
+        int n = s.size();
+        int ans = 0;
+        for(int i = 0;i<n;i++){
+            unordered_map<char,int>mp;            
+            for(int j = i;j<n;j++){
+                int maxVal = 0, minVal = INT_MAX; 
+                mp[s[j]]++;
+                for(auto &x:mp){
+                    //cout<<i<<" "<<x.first<<" "<<x.second<<endl;
+                    maxVal = max(maxVal, x.second);
+                    minVal = min(minVal,x.second);
+                }
+                ans+=maxVal-minVal;
+                //cout<<i<<" "<<ans<<endl;
+            }
+        }
+        return ans;
+    }
+};
