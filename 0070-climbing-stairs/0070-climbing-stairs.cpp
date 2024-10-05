73e2f172b07b045dcec56ac11500d70803d8bc01
Md Forhan Shahriar Fahim <dfahim432@gmail.com>
2024-10-05 09:30:55 +0600

    Time: 2 ms (48.53%), Space: 7.4 MB (89.54%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,13 +1,13 @@
 class Solution {
-public:    
-    int climbStairs(int n, unordered_map<int, int>& memo) {
+public:
+    int climbStairs(int n) {
+        int pre = 1, cur = 1;
         if(n == 0 || n == 1) return 1;
-        if(memo.find(n) == memo.end())
-            memo[n] = climbStairs(n-1, memo) + climbStairs(n-2, memo);
-        return memo[n];
-    }
-    int climbStairs(int n){
-        unordered_map<int, int>memo;
-        return climbStairs(n, memo);
+        for(int i = 2;i<=n;i++){
+            int temp = cur;
+            cur = pre + cur;
+            pre = temp;
+        }
+        return cur;
     }
 };
