1eb68b5f4bc4818a26282992a844c18bad7c9505
Md Forhan Shahriar Fahim <dfahim432@gmail.com>
2024-11-14 20:05:34 +0600

    Time: 11 ms (15.98%), Space: 14.6 MB (66.09%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -15,23 +15,46 @@ public:
 */
 
 class Solution {
-public:
-    Node* copyRandomList(Node* head) {
-        map<Node*, Node*>mpp;
+    Node* insertCopyBetween(Node* head){
         Node* temp = head;
         while(temp!=NULL){
-            Node* copyNode = new Node(temp->val);
-            mpp[temp] = copyNode;
-            temp = temp->next;
+            Node* newNode = new Node(temp->val);
+            newNode->next = temp->next; 
+            temp->next = newNode;
+            temp = temp->next->next;
         }
-        temp = head;
-
+        return head;
+    }
+    Node* connectRandomPointers(Node* head){
+        Node* temp = head;
         while(temp!=NULL){
-            Node* copyNode = mpp[temp];
-            copyNode->next = mpp[temp->next];
-            copyNode->random = mpp[temp->random];
+            Node* copyNode = temp->next;
+            if(temp->random){
+                copyNode->random = temp->random->next;
+            }else{
+                copyNode->random = NULL;
+            }
+            temp = temp->next->next;
+        }
+        return head;
+    }
+    Node* getDeepCopyList(Node* head){
+        Node* dummyNode = new Node(-1);
+        Node* temp = head, *res = dummyNode;
+        while(temp!=NULL){
+            res->next = temp->next;
+            res = res->next;
+
+            temp->next = temp->next->next;
             temp = temp->next;
         }
-        return mpp[head];
+        return dummyNode->next;
+    }
+public:
+    Node* copyRandomList(Node* head) {
+        if(head == NULL) return head;
+        insertCopyBetween(head);
+        connectRandomPointers(head);
+        return getDeepCopyList(head);        
     }
 };
