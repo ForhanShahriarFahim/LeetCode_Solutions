2c40e80e4ac5cbee758003098bd5e95b8d908485
Md Forhan Shahriar Fahim <dfahim432@gmail.com>
2024-11-30 14:06:57 +0600

    Time: 3 ms (72.33%), Space: 10.9 MB (8.18%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,23 +1,19 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        int l = 0, r = 0, maxLen = 0, maxF = 0;
-        map<int,int>hashmap;
+        int l = 0, r = 0, maxL = 0, maxF = 0;
+        unordered_map<int, int>hashmap;
         while(r<s.size()){
             hashmap[s[r]-'A']++;
             maxF = max(maxF, hashmap[s[r]-'A']);
-            while((r-l+1) - maxF> k){
+            if((r-l+1) - maxF>k){
                 hashmap[s[l]-'A']--;
-                maxF = 0;
-                for(auto x:hashmap){
-                    maxF = max(maxF, x.second);
-                }
-                l+=1;
+                l++;
             }
             if((r-l+1)-maxF<=k)
-                maxLen = max(maxLen, r-l+1);
+                maxL = max(maxL, r-l+1);
             r++;
         }
-        return maxLen;
+        return maxL;
     }
 };
