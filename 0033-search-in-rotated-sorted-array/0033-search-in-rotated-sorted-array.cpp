3f11f2a159d8f062786866ccc4114c0865796511
Md Forhan Shahriar Fahim <dfahim432@gmail.com>
2024-12-08 23:59:01 +0600

    Time: 0 ms (100%), Space: 15 MB (14.13%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,34 +1,26 @@
 class Solution {
-private: 
-    int rotatedBinarySearch(vector<int>&nums,int low, int high, int target){
-        int ans = -1;
-        while(low<=high){
-            int mid = (low+high)/2;
-            if(nums[mid] == target){
-                
-                return mid;
-            }
-            if(nums[low]<=nums[mid]){
-                if(nums[low]<=target && nums[mid]>=target){
-                    high = mid-1;
-                }
-                else{
-                    low = mid+1;
-                }
-            }
-            else {
-                if(nums[mid]<=target &&target<=nums[high]){
-                    low = mid+1;
-                }
-                else {
-                    high = mid-1;
-                }
-            }
-        }
-        return -1;
-    }
 public:
     int search(vector<int>& nums, int target) {
-       return rotatedBinarySearch(nums,0,nums.size()-1, target);
+        int low = 0, high = nums.size()-1;
+        int ans = -1;
+        while(low<=high){
+            int mid = (high + low)/2;
+            if(nums[mid] == target) return mid;
+            if(nums[low]<=nums[mid]){
+                if(nums[low]<=target && target<=nums[mid]){
+                    high = mid - 1;
+                }else {
+                    low = mid + 1;
+                }
+            }else{
+                if(nums[mid]<=target && target<=nums[high]){
+                    low = mid + 1;
+                }else{
+                    high = mid -1;
+                }
+            }
+
+        }
+        return ans;
     }
 };
