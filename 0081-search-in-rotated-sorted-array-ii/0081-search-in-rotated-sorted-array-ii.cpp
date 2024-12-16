2fd1c34e6f91a63619776665369d149d348df0cc
Md Forhan Shahriar Fahim <dfahim432@gmail.com>
2024-12-16 17:53:26 +0600

    Time: 4 ms (5.68%), Space: 17.9 MB (16.53%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -1,35 +1,25 @@
 class Solution {
-private:
-    bool rotatedBinarySearch(vector<int>&nums,int high,int target){
-        int low = 0;
+public:
+    bool search(vector<int>& nums, int target) {
+        int low = 0, high = nums.size()-1;
         while(low<=high){
-            int mid = (low+high)/2;
-            if(nums[mid] == target){
-                return true;
-            }
+            int mid = (high + low)/2;
+            if(nums[mid] == target) return true;
             if(nums[low] == nums[mid] && nums[mid] == nums[high]){
                 low++;
                 high--;
+                cout<<low<<" "<<high<<endl;
                 continue;
             }
-            if(nums[low]<=nums[mid]){
-                if(nums[low]<=target && target<=nums[mid]){
-                    high = mid-1;
-                }
-                else low = mid+1;
-            }
-            else {
+            else if(nums[low]<=nums[mid]){
+                if(nums[low]<=target && target<=nums[mid]) high = mid - 1;
+                else low = mid + 1;
+            }else {
                 if(nums[mid]<=target && target<=nums[high]){
-                    low = mid+1;
-                }
-                else high = mid-1;
+                    low = mid + 1;
+                }else high = mid - 1;
             }
-        }
+        } 
         return false;
-        
-    }
-public:
-    bool search(vector<int>& nums, int target) {
-        return rotatedBinarySearch(nums,nums.size()-1,target);
     }
 };
