6922daf6f8bf6c71f1add6fb0afb066361e6b550
Md Forhan Shahriar Fahim <dfahim432@gmail.com>
2024-12-16 10:49:45 +0600

    Time: 0 ms (100%), Space: 15.3 MB (9.58%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -4,22 +4,16 @@ public:
         int low = 0, high = nums.size()-1;
         int ans = -1;
         while(low<=high){
-            int mid = (high + low)/2;
+            int mid = (low + high)/2;
             if(nums[mid] == target) return mid;
             if(nums[low]<=nums[mid]){
-                if(nums[low]<=target && target<=nums[mid]){
-                    high = mid - 1;
-                }else {
-                    low = mid + 1;
-                }
-            }else{
-                if(nums[mid]<=target && target<=nums[high]){
-                    low = mid + 1;
-                }else{
-                    high = mid -1;
-                }
+                if(nums[low]<=target && nums[mid]>=target) high = mid - 1;
+                else low = mid + 1;
             }
-
+            else {
+                if(nums[mid]<=target && nums[high]>=target) low = mid + 1;
+                else high = mid - 1;
+            }
         }
         return ans;
     }
