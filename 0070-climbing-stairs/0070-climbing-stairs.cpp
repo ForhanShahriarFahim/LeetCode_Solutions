6498065ee516986eaed041d265a6bef93730bd54
Md Forhan Shahriar Fahim <dfahim432@gmail.com>
2025-01-06 11:00:48 +0600

    Time: 0 ms (100%), Space: 8.5 MB (33.06%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,13 +1,24 @@
 class Solution {
 public:
-    int climbStairs(int n) {
-        int pre = 1, cur = 1;
-        if(n == 0 || n == 1) return 1;
-        for(int i = 2;i<=n;i++){
-            int temp = cur;
-            cur = pre + cur;
-            pre = temp;
-        }
-        return cur;
+    int topDown(int n, vector<int>&dp){
+        
+        if(n<2) return 1;
+        if(dp[n]!=-1) return dp[n];
+        return dp[n] = topDown(n - 1, dp) + topDown(n - 2, dp);
+    }
+     
+    int climbStairs(int n) {  
+        // bottom up
+        // vector<int>dp(n+2, 0);    
+        // dp[1] = 1;
+        // dp[2] = 2;
+        // for(int i=3;i<=n;i++){
+        //     dp[i] = dp[i - 1] + dp[i - 2];
+        // }  
+        // return dp[n];    
+
+        // Top Down
+        vector<int>dp(n + 1, -1);
+        return topDown(n, dp);
     }
 };
